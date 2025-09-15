d34b4645d711586277e4c5b3f0db73a6d72eeaf2
Brechtpd <Brechtp.devos@gmail.com>
2025-09-15 03:59:51 +0200

    don't state clear in multichain for pre execution changes

--- crates/evm/src/eth/block.rs
+++ crates/evm/src/eth/block.rs
@@ -101,8 +101,11 @@ where
 
     fn apply_pre_execution_changes(&mut self) -> Result<(), BlockExecutionError> {
         // Set state clear flag if the block is after the Spurious Dragon hardfork.
-        let state_clear_flag =
+        let mut state_clear_flag =
             self.spec.is_spurious_dragon_active_at_block(self.evm.block().number.saturating_to());
+        if self.ctx.len() > 1 {
+            state_clear_flag = false;
+        }
         self.evm.db_mut().set_state_clear_flag(state_clear_flag);
 
         println!("[alloy-evm] apply_pre_execution_changes from ctx");
